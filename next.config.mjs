159fada0d1a319848b98a91e61b676d338c30476
Aleksandr Mikhaylov <funnybulletgames@yandex.ru>
2025-02-16 18:30:58 +0300

    Fix icons v3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", basePath: "/cv_app" };
+const nextConfig = { output: "export", basePath: '/cv_app', images: { unoptimized: true, } };
 
 export default nextConfig;
